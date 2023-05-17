c0775363d12db0875aa6e6543490a8bc8d6dbecc
Matt Navarro <mattn@codev.com>
2023-05-17 18:56:11 +0800

    CFS-38-BE Post login action

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,6 +1,18 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@/models/User";
+import { ROLES } from "@/models/User/config";
+import type { IUser } from "@/models/User/types";
+
+import type { PickedUserDetails } from "@/features/auth/types";
+import { mongoConnect } from "@/middlewares/mongodb";
+
+const getDbUser = async (email: string): Promise<IUser | null> => {
+  await mongoConnect();
+  return (await User.findOne({ email: email })) as IUser | null;
+};
+
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
@@ -15,13 +27,48 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
   },
 
+  events: {
+    signIn: async ({ user }) => {
+      const email = user.email as string;
+      const dbUser = await getDbUser(email);
+
+      if (dbUser === null) {
+        const newUser: Pick<IUser, PickedUserDetails> = {
+          email,
+          isVerified: false,
+          role: ROLES.SURVEYEE,
+        };
+
+        await User.create(newUser);
+      }
+    },
+  },
+
   callbacks: {
+    signIn: ({ user }) => {
+      if (user.email?.endsWith("codev.com")) {
+        return true;
+      }
+
+      //Access denied
+      return false;
+    },
     jwt: async ({ token, user }) => {
       if (user) token.id = user.id;
-      return token;
+
+      //todo: find a more efficient way. if we can find a way not to hit the db, much better.
+      const email = token.email as string;
+      const dbUser = await getDbUser(email);
+      if (dbUser !== null) {
+        token.role = dbUser.role;
+      }
+      //end todo
+
+      return Promise.resolve(token);
     },
     session: async ({ session, token }) => {
       if (token) session.id = token.id;
+      session.user.role = token.role as string;
       return session;
     },
   },
