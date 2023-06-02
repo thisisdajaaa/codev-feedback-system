80fcccda7a6e9905e887756c70863e1f1dd81836
jbElmax <jan.elnas@gmail.com>
2023-06-02 14:29:03 +0800

    feat: searchbar initial integration

--- src/pages/home/components/SurveyorView.tsx
+++ src/pages/home/components/SurveyorView.tsx
@@ -8,6 +8,7 @@ import { AlertBanner } from "@/components/AlertBanner";
 import { Button } from "@/components/Button";
 import { Icon } from "@/components/Icon";
 import { Pagination } from "@/components/Pagination";
+import { SearchBar } from "@/components/SearchBar";
 import { SurveyCard } from "@/components/SurveyCard";
 import { surveyList } from "@/components/SurveyCard/config";
 import { Typography } from "@/components/Typography";
@@ -21,6 +22,10 @@ const SurveyorView: FC = () => {
   const [currentPage, setCurrentPage] = useState<number>(INITIAL_PAGE);
 
   const firstName = data?.user?.name?.split(" ")[0];
+  const handleSearch = (e: string) => {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  };
 
   const currentSurveyData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PAGE_SIZE;
@@ -48,6 +53,25 @@ const SurveyorView: FC = () => {
           handleClose={() => setShowAlert(false)}
         />
       </div>
+      <div className="mt-7 mb-[1.125rem] flex justify-end px-[1.125rem] sm:mb-[2.438rem] sm:px-0">
+        <Button className="flex w-full justify-center sm:w-auto">
+          <div className="text-[1.313rem]">
+            <Icon src="/assets/plus.svg" />
+          </div>
+
+          <Typography
+            variant="span"
+            size="text-lg"
+            lineHeight="leading-[1.688rem]"
+            textAlign="text-left"
+            color="text-white"
+            className="font-semibold"
+          >
+            Survey
+          </Typography>
+        </Button>
+      </div>
+      <SearchBar onSearch={handleSearch} />
 
       <div className="mt-7 mb-[27px] flex justify-end px-[1.125rem] sm:mb-[2.438rem] sm:px-0">
         <Button
