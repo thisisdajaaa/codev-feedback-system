9b90d7f22b0f2f45cda9714f5e8516d69eb1180e
jbElmax <jan.elnas@gmail.com>
2023-06-02 14:29:03 +0800

    feat: searchbar initial integration

--- src/pages/home/components/SurveyorView.tsx
+++ src/pages/home/components/SurveyorView.tsx
@@ -2,7 +2,10 @@ import { useSession } from "next-auth/react";
 import React, { FC, Fragment, useMemo, useState } from "react";
 
 import { AlertBanner } from "@/components/AlertBanner";
+import { Button } from "@/components/Button";
+import { Icon } from "@/components/Icon";
 import { Pagination } from "@/components/Pagination";
+import { SearchBar } from "@/components/SearchBar";
 import { SurveyCard } from "@/components/SurveyCard";
 import { surveyList } from "@/components/SurveyCard/config";
 import { Typography } from "@/components/Typography";
@@ -15,6 +18,10 @@ const SurveyorView: FC = () => {
   const [currentPage, setCurrentPage] = useState<number>(INITIAL_PAGE);
 
   const firstName = data?.user?.name?.split(" ")[0];
+  const handleSearch = (e: string) => {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  };
 
   const currentSurveyData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PAGE_SIZE;
@@ -42,13 +49,33 @@ const SurveyorView: FC = () => {
           handleClose={() => setShowAlert(false)}
         />
       </div>
+      <div className="mt-7 mb-[1.125rem] flex justify-end px-[1.125rem] sm:mb-[2.438rem] sm:px-0">
+        <Button className="flex w-full justify-center sm:w-auto">
+          <div className="text-[1.313rem]">
+            <Icon src="/assets/plus.svg" />
+          </div>
+
+          <Typography
+            variant="span"
+            size="text-lg"
+            lineHeight="leading-[1.688rem]"
+            textAlign="text-left"
+            color="text-white"
+            className="font-semibold"
+          >
+            Survey
+          </Typography>
+        </Button>
+      </div>
+      <SearchBar onSearch={handleSearch} />
 
       <div className="mx-auto mt-16 w-full max-w-screen-2xl bg-white pt-[1.063rem] pb-[3.375rem] shadow-md sm:rounded-2xl sm:px-6">
         <Typography
           variant="h2"
           color="text-gray-600"
           size="text-lg"
-          className="mb-[1.188rem] px-2 font-semibold sm:px-0">
+          className="mb-[1.188rem] px-2 font-semibold sm:px-0"
+        >
           My Surveys
         </Typography>
 
