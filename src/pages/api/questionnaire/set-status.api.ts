f551d03cc5c2f899fc6bb75fec41cd5b7f778316
Matt Navarro <mattn@codev.com>
2023-06-03 16:14:18 +0800

    feat: survey set-status endpoint

--- src/pages/api/questionnaire/set-status.api.ts
+++ src/pages/api/questionnaire/set-status.api.ts
@@ -1,27 +1,15 @@
-import type { NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
 
 import { ROLES } from "@/models/User/config";
 
-import type { ApiResponse } from "@/types";
-
 import { QuestionnaireController } from "@/features/questionnaire";
-import type {
-  CreatedQuestionnaireResponse,
-  GetQuestionnaireResponse,
-  ICreateQuestionnaireRequest,
-} from "@/features/questionnaire/types";
 import { onError } from "@/middlewares/errors";
 import { isAuthenticatedUser } from "@/middlewares/isAuthenticatedUser";
 import { mongoHandler } from "@/middlewares/mongodb";
 import { roleAtLeast } from "@/middlewares/roleAtLeast";
 
-const handler = nextConnect<
-  ICreateQuestionnaireRequest,
-  NextApiResponse<
-    ApiResponse<CreatedQuestionnaireResponse | GetQuestionnaireResponse[]>
-  >
->({
+const handler = nextConnect<NextApiRequest, NextApiResponse>({
   onError,
 });
 
