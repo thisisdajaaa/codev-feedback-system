dd9c779818c2b15437356229cdd59b2f61807d4e
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2023-06-22 13:52:45 +0800

    feat: commented out survey status

--- src/components/SurveyCard/SurveyCard.tsx
+++ src/components/SurveyCard/SurveyCard.tsx
@@ -27,7 +27,8 @@ const SurveyCard: FC<SurveyProps> = (props) => {
   const isSurveyDraft = surveyStatus.toUpperCase() === "DRAFT";
   const isSurveyClosed = surveyStatus.toUpperCase() === "FINISHED";
 
-  const surveyStatusPercentage = Math.floor(responses * totalRespondents) / 100;
+  const _surveyStatusPercentage =
+    Math.floor(responses * totalRespondents) / 100;
 
   const surveyCardBtnClassNames =
     "h-[1.375rem] min-w-[5.375rem] rounded-md py-4 px-8";
@@ -40,7 +41,8 @@ const SurveyCard: FC<SurveyProps> = (props) => {
         isSurveyDraft &&
           "before:border-r-[0.063rem] before:border-gray-500 before:bg-white",
         isSurveyClosed && "before:bg-gray-500"
-      )}>
+      )}
+    >
       <div
         className={clsxm(
           "absolute -top-1 -right-1 min-w-[5rem] py-2.5 px-3.5 text-center text-white",
@@ -48,7 +50,8 @@ const SurveyCard: FC<SurveyProps> = (props) => {
           isSurveyDraft &&
             "border-[0.063rem] border-gray-500 bg-white text-gray-500",
           isSurveyClosed && "bg-gray-500"
-        )}>
+        )}
+      >
         {surveyStatus}
       </div>
 
@@ -74,17 +77,21 @@ const SurveyCard: FC<SurveyProps> = (props) => {
         )}
 
         <p className="mt-2 text-xs text-gray-500">Survey Status:</p>
-        <p className="text-xs">
+        {/**
+         * TODO: Uncomment once survey status calculation has been added
+         */}
+        {/* <p className="text-xs">
           {surveyStatusPercentage}% - {responses} of {totalRespondents}{" "}
           participants
-        </p>
+        </p> */}
       </div>
 
       <div
         className={clsxm(
           "flex flex-col gap-[0.938rem] md:flex-row md:justify-between",
           isSurveyClosed && "justify-end"
-        )}>
+        )}
+      >
         {isSurveyActive && onInvite && (
           <Button
             variant="primary"
@@ -92,7 +99,8 @@ const SurveyCard: FC<SurveyProps> = (props) => {
               surveyCardBtnClassNames,
               "border-solid border-gray-500 bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-500 active:bg-gray-100"
             )}
-            onClick={() => onInvite(templateId)}>
+            onClick={() => onInvite(templateId)}
+          >
             <span className="inline-block w-full text-center text-sm font-normal">
               Invite
             </span>
@@ -103,7 +111,8 @@ const SurveyCard: FC<SurveyProps> = (props) => {
           <Button
             variant="warning"
             onClick={onDelete}
-            className={surveyCardBtnClassNames}>
+            className={surveyCardBtnClassNames}
+          >
             <span className="inline-block w-full text-center text-sm font-normal">
               Delete
             </span>
@@ -114,7 +123,8 @@ const SurveyCard: FC<SurveyProps> = (props) => {
           <Button
             variant="primary"
             onClick={onPrimaryAction}
-            className={clsxm(surveyCardBtnClassNames, "md:ml-auto")}>
+            className={clsxm(surveyCardBtnClassNames, "md:ml-auto")}
+          >
             <span className="inline-block w-full text-center text-sm font-normal">
               {isSurveyActive ? "Answer" : "View"}
             </span>
@@ -125,7 +135,8 @@ const SurveyCard: FC<SurveyProps> = (props) => {
           <Button
             variant="primary"
             onClick={onPrimaryAction}
-            className={clsxm(surveyCardBtnClassNames, "md:ml-auto")}>
+            className={clsxm(surveyCardBtnClassNames, "md:ml-auto")}
+          >
             <span className="inline-block w-full text-center text-sm font-normal">
               {isSurveyActive || isSurveyClosed ? "Responses" : "Edit"}
             </span>
