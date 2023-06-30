20c7b9b01425dee0771cab478aa2f8cd5123d256
justinherrera <justinherrera013@yahoo.com>
2023-06-30 15:25:04 +0800

    feat: added navigation menu for mobile view with hamburger icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,6 +19,7 @@ const Navbar: FC = () => {
   const { pathname } = router;
 
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
 
   const handleLogout = async (
     event: React.MouseEvent<HTMLLIElement, MouseEvent>
@@ -29,18 +30,30 @@ const Navbar: FC = () => {
     await signOut();
   };
 
+  const handleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+    setIsNavOpen(false);
+  };
+
+  const handleNav = () => {
+    setIsDropdownOpen(false);
+    setIsNavOpen((prev) => !prev);
+  };
+
   return (
     <nav className="fixed z-10 flex h-[4.5rem] w-full justify-between bg-white px-6 shadow-md">
-      <div className="flex items-center xs:invisible sm:visible md:visible">
+      <div className="flex items-center">
         <Image
           src="/assets/codev-logo.svg"
           width={32}
           height={32}
           alt="codev logo"
         />
-        <h1 className="ml-3 text-[1.25rem]">Feedback System</h1>
+        <h1 className="ml-3 text-[1.2rem] sm:text-[1.25rem]">
+          Feedback System
+        </h1>
 
-        <ul className="ml-[3.625rem] flex h-full">
+        <ul className="ml-[3.625rem] flex hidden h-full sm:visible">
           <li
             onClick={() => router.push(SYSTEM_URL.HOME)}
             className={clsxm(
@@ -77,59 +90,91 @@ const Navbar: FC = () => {
         </ul>
       </div>
 
-      <div
-        onClick={() => setIsDropdownOpen((prev) => !prev)}
-        className="relative flex cursor-pointer items-center gap-3"
-      >
-        <Image
-          src={data?.user.image || "/assets/avatar-placeholder.svg"}
-          width={28}
-          height={28}
-          className="rounded-full"
-          alt="avatar"
-        />
+      <div className="flex items-center">
+        <div
+          onClick={handleDropdown}
+          className="relative mr-3 flex cursor-pointer items-center gap-3"
+        >
+          <Image
+            src={data?.user.image || "/assets/avatar-placeholder.svg"}
+            width={28}
+            height={28}
+            className="rounded-full"
+            alt="avatar"
+          />
 
-        <Image
-          className={clsxm(
-            "ease 300ms transition",
-            isDropdownOpen ? "rotate-180" : ""
-          )}
-          src="/assets/chevron-down.svg"
-          width={12}
-          height={6}
-          alt="chevron down icon"
-        />
+          <Image
+            className={clsxm(
+              "ease 300ms transition",
+              isDropdownOpen ? "rotate-180" : ""
+            )}
+            src="/assets/chevron-down.svg"
+            width={12}
+            height={6}
+            alt="chevron down icon"
+          />
 
-        {isDropdownOpen && (
-          <div className="absolute top-[4.5rem] -right-6 z-20 border bg-white">
-            <ul className="min-w-[13.5rem]">
-              <li className="bg-gray-100 p-4">
-                {data?.user.name}
-                <small className="block text-ellipsis whitespace-nowrap text-xs text-gray-500">
-                  signed in as {data?.user.email}
-                </small>
-              </li>
-
-              {navLinks.map((nav, index) => (
-                <li
-                  key={index}
-                  onClick={() => router.push(nav.url)}
-                  className="p-4 text-gray-600 hover:bg-gray-100 active:bg-gray-100"
-                >
-                  {nav.label}
+          {isDropdownOpen && (
+            <div className="absolute top-[3.5rem] -right-6 z-20 border bg-white">
+              <ul className="min-w-[13.5rem]">
+                <li className="bg-gray-100 p-4">
+                  {data?.user.name}
+                  <small className="block text-ellipsis whitespace-nowrap text-xs text-gray-500">
+                    signed in as {data?.user.email}
+                  </small>
                 </li>
-              ))}
 
-              <li
-                className="p-4 text-gray-600 hover:bg-gray-100 active:bg-gray-100"
-                onClick={handleLogout}
-              >
-                <span>Log Out</span>
+                {navLinks.map((nav, index) => (
+                  <li
+                    key={index}
+                    onClick={() => router.push(nav.url)}
+                    className="p-4 text-gray-600 hover:bg-gray-100 active:bg-gray-100"
+                  >
+                    {nav.label}
+                  </li>
+                ))}
+
+                <li
+                  className="p-4 text-gray-600 hover:bg-gray-100 active:bg-gray-100"
+                  onClick={handleLogout}
+                >
+                  <span>Log Out</span>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div
+          onClick={handleNav}
+          className={`visible relative flex cursor-pointer items-center gap-3 sm:hidden ${
+            isNavOpen ? "bg-gray-100" : ""
+          }`}
+        >
+          <Image
+            src="/assets/hamburger.svg"
+            width={32}
+            height={32}
+            alt="codev logo"
+          />
+        </div>
+      </div>
+      {isNavOpen && (
+        <div className="absolute top-[5rem] left-2 z-20 w-[96%] rounded-lg border bg-white">
+          <div className="p-2">
+            <ul className="">
+              <li className="rounded bg-blue-500 p-[0.3em] pl-4 text-[1rem] font-light text-white">
+                Home
+              </li>
+              <li className="p-[0.3em] pl-4 text-[1rem] font-light">
+                Responses
+              </li>
+              <li className="p-[0.3em] pl-4 text-[1rem] font-light">
+                My Surveys
               </li>
             </ul>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </nav>
   );
 };
